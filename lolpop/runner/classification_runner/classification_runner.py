ef52939cd2dbb0517f2353aa23cde1209ad737c5
jordanvolz <jordan.volz@gmail.com>
2023-05-26 23:12:45 -0400

    wip

--- lolpop/runner/classification_runner/classification_runner.py
+++ lolpop/runner/classification_runner/classification_runner.py
@@ -166,7 +166,7 @@ class ClassificationRunner(BaseRunner):
         model_version, model, is_new_model_better = self.train_model(data, dataset_version)
         if is_new_model_better: 
             deployment = self.deploy_model(model_version)
-        eval_data, eval_dataset_version = self.process_data(source_data="eval")
+        eval_data, eval_dataset_version = self.process_data(source="eval")
         data, prediction_job = self.predict_data(model_version,model, eval_data, eval_dataset_version)
         self.evaluate_ground_truth(prediction_job)
     
